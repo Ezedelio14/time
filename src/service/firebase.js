cb829325263789c498e28235a3898c3a1c72c31b
Ezedelio14 <dinog165@gmail.com>
2023-05-01 12:43:36 +0100

    update file .env

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,33 +1,14 @@
 import * as firebase from "firebase/app";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDTttD7wlNoqL0BRrQA8vBcTzk7swixxRY",
-  authDomain: "time-319df.firebaseapp.com",
-  databaseURL: "https://time-319df-default-rtdb.firebaseio.com",
-  projectId: "time-319df",
-  storageBucket: "time-319df.appspot.com",
-  messagingSenderId: "153100075344",
-  appId: "1:153100075344:web:94f6cd63c86436c755ec55",
-  measurementId: "G-DJN5HZBH6V",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_API_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUTEMENT_ID,
 });
 
 export default app;
-
-/*
-export const setUsers = async (name, email, password) => {
-  await addDoc(userCollectionRef, { name, email, password });
-  return getUser(email, password);
-};
-
-export const getUser = async (email, password) => {
-  const data = await getDocs(userCollectionRef);
-  const users = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-  let userId = "";
-
-  users.map((user) => {
-    if (email === user.email) userId = user.id;
-  });
-
-  return userId;
-};
-*/
