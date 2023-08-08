390563ae3389d55c1c0f0da3d4ad1c9978aebd36
Ezedelio14 <dinog165@gmail.com>
2023-08-08 07:00:01 +0100

    update

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,11 +1,81 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Container from "./styles";
 import Logo from "../../components/Logo/index";
 import { Link } from "react-router-dom";
 import content from "../../content/content.json";
 
 const Landing = () => {
-  
+  const pagamento = async () => {
+    const reqs = await fetch("https://sandbox.api.pagseguro.com/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "SEU_TOKEN",
+      },
+      body: JSON.stringify({
+        reference_id: "ex-00001",
+        customer: {
+          name: "Jose da Silva",
+          email: "email@test.com",
+          tax_id: "12345678909",
+          phones: [
+            {
+              country: "55",
+              area: "11",
+              number: "999999999",
+              type: "MOBILE",
+            },
+          ],
+        },
+        items: [
+          {
+            name: "nome do item",
+            quantity: 1,
+            unit_amount: 500,
+          },
+        ],
+        qr_codes: [
+          {
+            amount: {
+              value: 500,
+            },
+            expiration_date: "2021-08-29T20:15:59-03:00",
+          },
+        ],
+        shipping: {
+          address: {
+            street: "Avenida Brigadeiro Faria Lima",
+            number: "1384",
+            complement: "apto 12",
+            locality: "Pinheiros",
+            city: "São Paulo",
+            region_code: "SP",
+            country: "BRA",
+            postal_code: "01452002",
+          },
+        },
+        notification_urls: ["https://meusite.com/notificacoes"],
+      }),
+    });
+  };
+
+  useEffect(() => {
+    // Make a GET request using the Fetch API
+    fetch("https://api.manjar.app.br/api/v1/users")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json(); // Parse the response body as JSON
+      })
+      .then((data) => {
+        console.log("Data:", data);
+      })
+      .catch((error) => {
+        console.error("Fetch Error:", error);
+      });
+  }, []);
+
   return (
     <Container>
       <div className="navbar">
